ce7e186ac655768165ab990fe9b1523aac919e9b
Maria Corrohcano <mariallombart@gmail.com>
2018-12-03 20:01:27 +0000

    Revert "updating"
    This reverts commit bfb0eb608381711013abe168ca0bcc2f887510d0.

--- java.js
+++ java.js
@@ -1,7 +1,12 @@
 $('.ye menubar').on('click', function(){
 <<<<<<< HEAD
+<<<<<<< HEAD
   $(hide('.containeryummy')
 }
+=======
+  $('.containeryummy').hide();
+});
+>>>>>>> parent of bfb0eb6... updating
 =======
   $('.containeryummy').hide();
 });
